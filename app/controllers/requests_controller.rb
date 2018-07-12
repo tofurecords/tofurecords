99d2bf5e1a6844d2282f7177ca67679339448742
tatameron <tatameron@gmail.com>
2018-07-12 05:47:05 +0000

    makotouser

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,16 @@
 class RequestsController < ApplicationController
-  def new
-  end
+	def new
+		@request =Request.new
+	end
+	def create
+		request = Request.new(request_params)
+		request.save
+		redirect_to new_request_path
+	end
+
+
+	private
+	def request_params
+		params.require(:request).permit(:artist_name,:song_name,:url)
+	end
 end
